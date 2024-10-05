fd214499e1f6e143bdfdcda55c45ab8c781df396
yexianglei <18810940932@163.com>
2024-10-05 16:27:03 +0800

    feat: update windows11 powershell profile

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,35 +1,68 @@
 # https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_profiles?view=powershell-7.4
 # ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1
 
+$Env:NVIM_CONFIG_HOME = "$Env:USERPROFILE\AppData\Local\nvim"
+
+$Env:POSH_HOME = "$Env:USERPROFILE\AppData\Local\Programs\oh-my-posh"
+$Env:POSH_THEMES_PATH = "$Env:POSH_HOME\themes"
+$Env:Path += ";$Env:POSH_HOME\bin"
+
+$Env:ANDROID_HOME = "$Env:USERPROFILE\AppData\Local\Android\Sdk"
+$Env:AVD_HOME = "$Env:USERPROFILE\.android\avd"
+$Env:JAVA_HOME = "C:\Program Files\Microsoft\jdk-17.0.12.7-hotspot"
+$Env:MAVEN_HOME = "C:\Program Files\apache-maven-3.9.9"
+$Env:GRADLE_HOME = "C:\Program Files\gradle-8.10"
+$Env:Path += ";$env:ANDROID_HOME\emulator;$env:ANDROID_HOME\platform-tools;$Env:MAVEN_HOME\bin;$Env:GRADLE_HOME\bin"
+
+$Env:SCOOP_HOME = "$Env:USERPROFILE\scoop"
+$Env:CHOCO_HOME = "C:\ProgramData\chocolatey"
+$Env:Path += ";$Env:SCOOP_HOME\shims;$Env:CHOCO_HOME\bin"
+
+$env:GO111MODULE = "on"
+$env:GOPROXY = "https://goproxy.cn"
+
+# $env:NVM_IOJS_ORG_MIRROR = "https://npm.taobao.org/mirrors/node"
+
+Import-Module PsFzf
 Import-Module posh-git
-Import-Module oh-my-posh
 Import-Module PSReadLine
 Import-Module z
 Import-Module Terminal-Icons
 
-# oh-my-posh init pwsh --config ~/Documents/oh-my-posh/themes/bubblesextra.omp.json | Invoke-Expression
-# oh-my-posh init pwsh --config ~/Documents/oh-my-posh/themes/catppuccin.omp.json | Invoke-Expression
-# oh-my-posh init pwsh --config ~/Documents/oh-my-posh/themes/easy-term.omp.json | Invoke-Expression
-oh-my-posh init pwsh --config ~/Documents/oh-my-posh/themes/gruvbox.omp.json| Invoke-Expression
-# oh-my-posh init pwsh --config ~/Documents/oh-my-posh/themes/kushal.omp.json| Invoke-Expression
-
+Set-PSReadlineOption -EditMode Emacs
 Set-PSReadLineOption -PredictionSource History
 Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
+Set-PSReadLineKeyHandler -Key Tab -ScriptBlock { Invoke-FzfTabCompletion }
 Set-PSReadLineKeyHandler -Key "Ctrl+p" -Function HistorySearchBackward
 Set-PSReadLineKeyHandler -Key "Ctrl+n" -Function HistorySearchForward
-Set-PSReadlineOption -EditMode Emacs
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 Set-Alias -Name v -Value nvim
 Set-Alias -Name g -Value git
 Set-Alias -Name l -Value lsd
-# Set-Alias -Name ls -Value lsd
+Set-Alias -Name p -Value pnpm
+Set-Alias -Name n -Value npm
 
 function which ($command) {
   Get-Command -Name $command -ErrorAction SilentlyContinue |
     Select-Object -ExpandProperty Path -ErrorAction SilentlyContinue
 }
 
-function p {pnpm}
-function v {nvim}
-winfetch
+# Import the Chocolatey Profile that contains the necessary code to enable
+# tab-completions to function for `choco`.
+# Be aware that if you are missing these lines from your profile, tab completion
+# for `choco` will not function.
+# See https://ch0.co/tab-completion for details.
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
+
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\bubblesextra.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\catppuccin.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\easy-term.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\gruvbox.omp.json" | Invoke-Expression
+# oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\kushal.omp.json" | Invoke-Expression
+
+# winfetch
+
--- README.md
+++ README.md
@@ -4,6 +4,33 @@
 
 https://repo.trojan-cdn.com/clash-verge/LatestRelease/
 
+## Setup for windows11 and WSL(Ubuntu)
+
+### Install Scoop(package manager)
+
+```powershell
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
+```
+
+For advanced installation options, check out the (Installer's Readme)[https://github.com/ScoopInstaller/Install#readme].
+
+### Install Prerequistites
+
+#### Glow
+> markdown reader for the terminal with a TUI
+
+```powershell
+scoop install main/glow
+```
+
+#### fzf
+```powershell
+scoop install fzf
+Install-Module -Name PsFzf -Scope CurrentUser -Force
+```
+
+
 ## Install nerd fonts
 
 ```shell
